3d45706a604029aebcbdb226bbbba6670a196710
Pei-Ying Kuan <go52dab@mytum.de>
2026-03-16 01:05:43 +0800

    20260316 01:05

--- report07.py
+++ report07.py
@@ -7,36 +7,36 @@ from scipy.optimize import curve_fit
 from scipy.interpolate import griddata
 
 
-def gaussian(xz, A, x0, z0, sig_x, sig_z, theta):
-    x, z = xz
-    x0 = float(x0)
-    z0 = float(z0)
-    a = (np.cos(theta)**2) / (2 * sig_x**2) + (np.sin(theta)**2) / (2 * sig_z**2)
-    b = -(np.sin(2 * theta)) / (4 * sig_x**2) + (np.sin(2 * theta)) / (4 * sig_z**2)
-    c = (np.sin(theta)**2) / (2 * sig_x**2) + (np.cos(theta)**2) / (2 * sig_z**2)
-    g = A * np.exp(-(a * ((x - x0)**2) + 2 * b * (x - x0) * (z - z0) + c * ((z - z0)**2)))
-    return g.ravel()
+# def gaussian(xz, A, x0, z0, sig_x, sig_z, theta):
+#     x, z = xz
+#     x0 = float(x0)
+#     z0 = float(z0)
+#     a = (np.cos(theta)**2) / (2 * sig_x**2) + (np.sin(theta)**2) / (2 * sig_z**2)
+#     b = -(np.sin(2 * theta)) / (4 * sig_x**2) + (np.sin(2 * theta)) / (4 * sig_z**2)
+#     c = (np.sin(theta)**2) / (2 * sig_x**2) + (np.cos(theta)**2) / (2 * sig_z**2)
+#     g = A * np.exp(-(a * ((x - x0)**2) + 2 * b * (x - x0) * (z - z0) + c * ((z - z0)**2)))
+#     return g.ravel()
 
-def fit(qx, qz, intensity):
-    A = intensity.max()
-    x0 = qx[np.argmax(intensity)]
-    z0 = qz[np.argmax(intensity)]
-    sig_x = 1
-    sig_z = 1
-    theta = 0
-    params, cov = curve_fit(gaussian, (qx, qz), intensity, p0=[A, x0, z0, sig_x, sig_z, theta])
-    A_, x0_, z0_, sig_x_, sig_z_, theta_ = params
-    return A_, x0_, z0_, sig_x_, sig_z_, theta_
+# def fit(qx, qz, intensity):
+#     A = intensity.max()
+#     x0 = qx[np.argmax(intensity)]
+#     z0 = qz[np.argmax(intensity)]
+#     sig_x = 1
+#     sig_z = 1
+#     theta = 0
+#     params, cov = curve_fit(gaussian, (qx, qz), intensity, p0=[A, x0, z0, sig_x, sig_z, theta])
+#     A_, x0_, z0_, sig_x_, sig_z_, theta_ = params
+#     return A_, x0_, z0_, sig_x_, sig_z_, theta_
 
 filepath = 'data/SampleB_ZnO_205_RSM.txt'
 
 # a = 0.32475
 # c = 0.52024 #nm
-wavelength = 1.54059 *10**-10
+wavelength = 0.154059 #nm
 h, k, l = 2, 0, 5
 
-qx = np.loadtxt(filepath, skiprows=2, encoding='utf-8')[:, 0] /(2*np.pi/wavelength)
-qz = np.loadtxt(filepath, skiprows=2, encoding='utf-8')[:, 1] /(2*np.pi/wavelength)
+qx = np.loadtxt(filepath, skiprows=2, encoding='utf-8')[:, 0] /(2/wavelength)
+qz = np.loadtxt(filepath, skiprows=2, encoding='utf-8')[:, 1] /(2/wavelength)
 intensity = np.loadtxt(filepath, skiprows=2, encoding='utf-8')[:, 2]
 
 # A, x0, z0, sig_x, sig_z, theta = fit(qx, qz, intensity)
@@ -63,8 +63,8 @@ qz0 = qz[np.argmax(intensity)]
 
 a = (1/qx0) * (wavelength*h/np.sqrt(3))
 c = (1/qz0) * (wavelength*l/2)
-print(a)
-print(c)
+print(abs(a))
+print(abs(c)) #nm
 
 plt.contourf(QX, QZ, intensity_grid, levels=20, cmap='viridis')
 plt.scatter(qx0, qz0, color='red', label='peak', s=1)
